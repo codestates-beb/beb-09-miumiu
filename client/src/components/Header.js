60ea81f5490a34712fc29395e7947670d4c6b7bb
KimSeoYeon23 <sollucidus0221@naver.com>
2023-06-26 15:51:45 +0900

    metamask 연결 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import InputBase from '@mui/material/InputBase';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
+import { 
+  Grid, 
+  InputBase, 
+  Button, 
+  IconButton, 
+  InputAdornment, 
+  Dialog, 
+  DialogActions, 
+  DialogContent, 
+  DialogContentText, 
+  DialogTitle 
+} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import WalletIcon from '@mui/icons-material/Wallet';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Web3 from 'web3';
 import '../assets/css/Header.css';
 
 export default function Header () {
-
+  let web3;
+  if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    web3 = new Web3(window.ethereum);
+  }
   const [searchTerm, setSearchTerm] = useState("");
+  const [account, setAccount]  = useState('');
+  const [open, setOpen] = useState(false); // 추가된 부분
+
+  useEffect(() => {
+    if(typeof window.ethereum !== 'undefined') {
+      console.log('MetaMask is installed!');
+    }
+  })
+
+  const LoginWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert('Please install MetaMask!');
+    }
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const Logout = () => {
+    setAccount(null);
+    setOpen(false); // 추가된 부분
+  };
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -65,18 +110,60 @@ export default function Header () {
           </form>
         </Grid>
         <Grid item container justifyContent={"flex-end"} xs={3} className="headerUser">
-          <Button className="walletBtn" 
-            sx={{
-            color: 'white', 
-            bgcolor: 'rgba(255, 255, 255, 0.2)', 
-            borderRadius: '10px',
-            ':hover': {bgcolor: 'rgba(255, 255, 255, 0.4)'},
-            padding: '15px',
-            marginRight: '10px'
-            }}>
-            <WalletIcon sx={{ marginRight: '10px' }}/>
-            Connect Wallet
-          </Button>
+          {
+            !account ? (
+              <Button className="walletBtn" 
+                sx={{
+                color: 'white', 
+                bgcolor: 'rgba(255, 255, 255, 0.2)', 
+                borderRadius: '10px',
+                ':hover': {bgcolor: 'rgba(255, 255, 255, 0.4)'},
+                padding: '15px',
+                marginRight: '10px'
+                }}
+                onClick={LoginWallet}
+                >
+                <WalletIcon sx={{ marginRight: '10px' }}/>
+                Connect Wallet
+              </Button>
+            ) : (
+              <>
+                <Button className="walletBtn" 
+                  sx={{
+                  color: 'white', 
+                  bgcolor: 'rgba(255, 255, 255, 0.2)', 
+                  borderRadius: '10px',
+                  ':hover': {bgcolor: 'rgba(255, 255, 255, 0.4)'},
+                  padding: '15px',
+                  marginRight: '10px'
+                  }}
+                  onClick={handleClickOpen}
+                  >
+                  <WalletIcon sx={{ marginRight: '10px' }}/>
+                  {account.slice(0, 13) + '...'}
+                </Button>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  <DialogTitle id="alert-dialog-title">{"Confirm Logout"}</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                      Are you sure you want to logout?
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={Logout} autoFocus>
+                      Logout
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </>
+            )
+          }
           <Grid item>
             <Button className="walletBtn" 
               sx={{
