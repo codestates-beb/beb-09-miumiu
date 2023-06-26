53bc98657aada27b1cb9c46c181c552cf6264718
KimSeoYeon23 <sollucidus0221@naver.com>
2023-06-26 10:19:09 +0900

    충돌 삭제

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,17 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import NftList from '../components/NftList';
-=======
->>>>>>> ff15ae546a7a2b59ed15706275c7bfce98653dd7
 
 export default function Main () {
 
   return (
     <div>
-<<<<<<< HEAD
       <NftList />
-=======
->>>>>>> ff15ae546a7a2b59ed15706275c7bfce98653dd7
     </div>
   )
 }
