9438a590330927041d5f98f5c083f09882b7a4f8
leejs <leejis0212@icloud.com>
2023-06-29 21:22:25 +0900

    서버 서명 검증 추가

--- server/index.js
+++ server/index.js
@@ -20,19 +20,40 @@ app.get("/", (req, res) => {
 app.post("/create", articleFormDataHandler, async (req, res) => {
   // console.log("req.body", req.body);
   console.log("req.files", req.files);
+
   const { img } = req.files;
-  const { title, exLink, description, category, price } = req.body;
-  //@args 이미지 경로, 이미지 이름, 이미지 타입, 제목, 링크, 설명, 카테고리
-  storeNFT(
-    img[0].destination + img[0].path,
-    img[0].filename,
-    img[0].mimetype,
+  const {
     title,
     exLink,
     description,
-    category
-  );
-  res.send("ok");
+    category,
+    price,
+    signature,
+    message,
+    userAddress,
+  } = req.body;
+
+  try {
+    //@args 이미지 경로, 이미지 이름, 이미지 타입, 제목, 링크, 설명, 카테고리
+    const resultUri = await storeNFT(
+      img[0].destination + img[0].path,
+      img[0].filename,
+      img[0].mimetype,
+      title,
+      exLink,
+      description,
+      category,
+      price,
+      signature,
+      message,
+      userAddress
+    );
+
+    res.json({ resultUri });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Failed to store NFT");
+  }
 });
 
 const server = http.createServer(app);
--- server/loaders/multer.js
+++ server/loaders/multer.js
@@ -3,17 +3,16 @@ import multer from "multer";
 // diskStorage를 사용할 경우
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './')
+    cb(null, "./");
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
-
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
 
 // memoryStorage를 사용할 경우
 //const multerStorage = multer.memoryStorage();
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 // multer 라이브러리가 현재 텍스트를 latin1 으로 처리하여 한글로된 파일 이름이 깨지는 문제가 있다.
 // Parser 함수를 따로 구현하여 사용하도록 한다.
@@ -28,4 +27,7 @@ export const articleFormDataHandler = upload.fields([
   { name: "description", maxCount: 1 },
   { name: "category", maxCount: 1 },
   { name: "price", maxCount: 1 },
+  { name: "signature", maxCount: 1 },
+  { name: "message", maxCount: 1 },
+  { name: "userAddress", maxCount: 1 },
 ]);
