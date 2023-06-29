e6d07c629d6e678593cacf5b14079525d25845a7
KimSeoYeon23 <sollucidus0221@naver.com>
2023-06-30 08:19:59 +0900

    detail페이지 완성

--- client/src/components/Detail/Detail.js
+++ client/src/components/Detail/Detail.js
@@ -1,105 +1,57 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom'
 import styles from '../../assets/css/Detail.module.css';
 import image2 from "../../assets/images/dummy2.png";
 import { get721Contract } from '../../Contract/Contract'
 import { Context } from '../../Context/index';
 
-
 const Detail = () => { 
-    const imgurl = image2
-    const title = "ether"
-    const owned = "5E97A8"
-    const price = "0.04"
-    const Description = "WonderPals is a collection of 10,000 delightfully cute pals. Our vision is to bring more joy to the world through community, creativity, and fun art!"
-    const category = "Art"
-    const exlink = "http://aaa.com"
-    const extension = "png"
-    const id = 0
+
+  const imgurl = image2
+  const title = "ether"
+  const owned = "5E97A8"
+  const price = "0.04"
+  const Description = "WonderPals is a collection of 10,000 delightfully cute pals. Our vision is to bring more joy to the world through community, creativity, and fun art!"
+  const category = "Art"
+  const exlink = "http://aaa.com"
+  const extension = "png"
+  const location = useLocation();
+
+  const info = location.state.info;
+
+  console.log(info);
 
     // user info
-    const { state: { user }, dispatch } = useContext(Context);
-    const [NftList, setNftList] = useState([]);
+  const { state: { user }, dispatch } = useContext(Context);
+  const [NftList, setNftList] = useState([]);
   const [jsonData, setJsonData] = useState([]);
-  const [NftUrl, setNftUrl] = useState()
-  
-  
-
-  const getAnimalTokens = async () => {
-    let contractAddress = process.env.REACT_APP_ERC_721_ADDRESS;
-
-    try {
-      // const response = await get721Contract(contractAddress).methods.getNftTokenList(user.account).call();
-      const response = await get721Contract(contractAddress).methods.getAllNftList().call();
-      //console.log(response)
-      setNftList(prevList => [...prevList, ...response]);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-    useEffect(() => {
-      getAnimalTokens()
-    }, []);
-
-    // NftList.map((el, index) => {
-    //   setNftUrl(el[1])
-    //   console.log(el[1])
-    // })
-    
-
-    
+  const [NftUrl, setNftUrl] = useState();
+  const [selectedNft, setSelectedNft] = useState(null);
 
     const IpfsParser = (url) => {
+      if (!url) {
+        return;
+      }
       const cid = url.slice(7,url.length)
       const ipfsUrl = "https://ipfs.io/ipfs/" + cid
       console.log(ipfsUrl)
       return ipfsUrl
     }
     
-
-    
-    
-    useEffect(() => {
-      const fetchData = async (url) => {
-        try {
-          const response = await fetch(IpfsParser(url));
-          const data = await response.json();
-          console.log(data)
-          setJsonData(data)
-          // const image = data['image'];
-          // setJsonData(prevData => [...prevData, image]);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-    
-      const fetchAllData = async () => {
-        const url = NftList[id]
-        await fetchData(url[1]);
-        
-        console.log("jsonData", jsonData);
-      };
-    
-      fetchAllData();
-    }, [NftList]);
-    
-    console.log("jsonData", jsonData)
-    
-
   return (
 
     <>
       <div className={styles.detailContainer}>
         <div className={styles.left}>
-            <div>
-            <img src={IpfsParser(jsonData['image'])} alt="dummy image" className={styles.squareImage}></img>
+            <div className={styles.imageContainer}>
+              <img src={IpfsParser(info.image)} alt="NFT image" className={styles.squareImage} />
             </div>
             
+            
         </div>
-
         <div className={styles.right}>
             <div className={styles.titleContainer}>
-                <h1 className={`${styles.title} ${styles.owned} ${styles.titleSize}`}>{jsonData['name']}</h1>
+                <h1 className={`${styles.title} ${styles.owned} ${styles.titleSize}`}>{info.name}</h1>
                 <p className={`${styles.title} ${styles.owned}`}>Owned by {owned}</p>
             </div>
             
@@ -107,7 +59,7 @@ const Detail = () => {
             <div className={styles.priceContainer}>
                 <div>
                     <span>Current price</span>
-                    <h1 className={styles.priceVal}>{jsonData['price']}ETH</h1>
+                    <h1 className={styles.priceVal}>{Number(info.price)}ETH</h1>
                 </div>
                 <button className={styles.buyBtn}><h3>Buy now</h3></button>
             </div>
@@ -115,22 +67,18 @@ const Detail = () => {
             <div className={styles.Description}>
                 <h3 className={styles.DescriptionH3}>Description</h3>
                 <div className={styles.DescriptionLine}></div> {/* Add a separate div for the line */}
-                <p className={styles.DescriptionP}>{jsonData['description']}</p>
+                <p className={styles.DescriptionP}>{info.description}</p>
                 <div className={styles.DescriptionLine}></div>
                 <h3 className={styles.DescriptionDitails}>Details</h3>
                 <div className={styles.DescriptionLine}></div>
                 <div className={styles.DescriptionVal}>
                     <div >
                     Category
-                    <span className={styles.rightAlign}>{category}</span>
+                    <span className={styles.rightAlign}>{info.properties.category}</span>
                     </div>
                     <div>
                     ExLink
-                    <span className={styles.rightAlign}>{exlink}</span>
-                    </div>
-                    <div>
-                    Extension
-                    <span className={styles.rightAlign}>{extension}</span>
+                    <span className={styles.rightAlign}>{info.properties.exLink}</span>
                     </div>
                 </div>
             </div>
--- client/src/components/Main/MainSlide.js
+++ client/src/components/Main/MainSlide.js
@@ -1,5 +1,6 @@
 import React , { useContext, useEffect, useState }from 'react';
 import Slider from "react-slick";
+import { Link } from 'react-router-dom';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import image1 from "../../assets/images/dummy1.jpeg";
@@ -28,6 +29,7 @@ const MainSlide = () => {
   const [NftList, setNftList] = useState([]);
   const [jsonData, setJsonData] = useState([]);
   const [NftUrl, setNftUrl] = useState();
+  const [infoNft, setInfoNft] = useState([]);
   
   
 
@@ -38,155 +40,72 @@ const MainSlide = () => {
       // const response = await get721Contract(contractAddress).methods.getNftTokenList(user.account).call();
       const response = await get721Contract(contractAddress).methods.getAllNftList().call();
       console.log('response', response)
-      setNftList(prevList => [...prevList, ...response]);
+      // setNftList(prevList => [...prevList, ...response]);
+      setNftList(response.map(nft => [Number(nft[0]), nft[1]]));
     } catch (error) {
       console.error(error);
     }
   }
   
-    useEffect(() => {
-      getAnimalTokens()
-    }, []);
+  useEffect(() => {
+    getAnimalTokens()
+  }, []);
 
-    // NftList.map((el, index) => {
-    //   setNftUrl(el[1])
-    //   console.log(el[1])
-    // })
+  const IpfsParser = (url) => {
+    const cid = url.slice(7,url.length)
+    const ipfsUrl = "https://ipfs.io/ipfs/" + cid
+    console.log(ipfsUrl)
+    return ipfsUrl
+  }
     
-
-    
-
-    const IpfsParser = (url) => {
-      const cid = url.slice(7,url.length)
-      const ipfsUrl = "https://ipfs.io/ipfs/" + cid
-      console.log(ipfsUrl)
-      return ipfsUrl
-    }
-    
-
-    // useEffect(() => {
-    //   const fetchData = async (url) => {
-    //     try {
-    //       const response = await fetch(IpfsParser(url));
-    //       const data = await response.json();
-    //       const image = data['image'];
-    //       setJsonData(prevData => [...prevData, image]);
-    //     } catch (error) {
-    //       console.error(error);
-    //     }
-    //   };
-    
-    //   const fetchAllData = async () => {
-    //     for (const data of NftList) {
-    //       await fetchData(data[1]);
-    //     }
-    //     console.log("jsonData", jsonData);
-    //   };
-    
-    //   fetchAllData();
-    // }, [NftList]);
-
-    // useEffect(() => {
-    //   const fetchData = async (url) => {
-    //       try {
-    //           const response = await fetch(IpfsParser(url));
-    //           const data = await response.json();
-    //           return IpfsParser(data['image']);  // IPFS URL을 반환하도록 변경
-    //       } catch (error) {
-    //           console.error(error);
-    //       }
-    //   };
-  
-    //   const fetchAllData = async () => {
-    //       try {
-    //           const allData = await Promise.all(NftList.map(data => fetchData(data[1])));
-    //           setJsonData(allData);  // allData는 이미 IPFS URL의 배열입니다.
-    //           console.log('jsonData', allData);
-    //       } catch (error) {
-    //           console.error(error);
-    //       }
-    //   };
-  
-    //   fetchAllData();
-    // }, [NftList]);
-    useEffect(() => {
-      const fetchData = async (url) => {
-          try {
-              const response = await fetch(IpfsParser(url));
-              const data = await response.json();
-              return IpfsParser(data['image']);  // IPFS URL을 반환하도록 변경
-          } catch (error) {
-              console.error(error);
-          }
+  useEffect(() => {
+    const fetchData = async (url) => {
+        try {
+          const response = await fetch(IpfsParser(url));
+          const data = await response.json();
+          setInfoNft(prevList => [...prevList, ...(Array.isArray(data) ? data : [data])]);
+          // return data;  
+        } catch (error) {
+            console.error(error);
+        }
       };
   
-      const fetchAllData = async () => {
-          try {
-              const allData = await Promise.all(NftList.map(data => fetchData(data[1])));
-              setJsonData(allData);  // allData는 'image' 속성의 값의 배열입니다.
-              console.log('jsonData', allData);
-          } catch (error) {
-              console.error(error);
-          }
+    const fetchAllData = async () => {
+      try {
+        const allData = await Promise.all(NftList.map(data => fetchData(data[1])));
+        setJsonData(allData);  // allData는 'image' 속성의 값의 배열입니다.
+        console.log('jsonData', allData);
+      } catch (error) {
+          console.error(error);
+      }
       };
   
       fetchAllData();
     }, [NftList]);
+
+    console.log('infoNft', infoNft);
   
-    return (
-      <>
-        <Slider className={styles.mainSlide} {...settings}>
-          {jsonData.map((data, i) => {
-            console.log("data", data)
-            return (
-              <div className={styles.imgWrap} key={i}>
-                <img src={data} alt={`Image ${i}`}/>
-              </div>
-            )
-          })}
-          <div className={styles.imgWrap}>
-            <img src={IpfsParser('ipfs://bafybeigqxcwjlwmgkhg4i6sq6xptd4nfgext5kg5qxzrpap6nmzp6nwsli/img-1688047924434')} alt="Image 1"/>
-          </div>
-          {/* <div className={styles.imgWrap}>
-            <img src={image1} alt="Image 1"/>
-          </div>
-          <div className={styles.imgWrap}>
-            <img src={image1} alt="Image 1"/>
-          </div>
-          <div className={styles.imgWrap}>
-            <img src={image2} alt="Image 2" />
-          </div>
-          <div className={styles.imgWrap}>
-            <img src={image3} alt="Image 3" />
-          </div> */}
-          {/* <div className={styles.imgWrap}>
-            <img src={image3} alt="Image 3" />
-          </div> */}
-          {/* <div className={styles.imgWrap}>
-            <img src={image3} alt="Image 3" />
-          </div> */}
-        </Slider>
-      </>
-    )
-  
-    
-    
-  // return (
-  //   <>
-  //     <Slider className={styles.mainSlide} {...settings}>
-  //       {jsonData.map((data, i) => {
-  //         console.log("data",IpfsParser(data))
-  //         return(
-  //         <div className={styles.imgWrap} key={i}>
-  //           <img src={IpfsParser(data)} alt="Image 1"/>
-  //       </div>
-  //         )
-  //       })}
-  //       
-  //       {/* 추가 이미지 */}
-  //     </Slider>
-  //   </>
-  // )
+  return (
+    <>
+      <Slider className={styles.mainSlide} {...settings}>
+        {infoNft.map((data, i) => {
+          console.log("data", data, i)
+          return (
+            <div className={`${styles.imgWrap} ${styles.nftImage}`} key={i}>
+              {/* {console.log(NftList[i][0])} */}
+              <Link to={`/detail/${i}`} state={{ info: data }}>
+                <img src={IpfsParser(data.image)} alt={`Image ${i}`}/>
+              </Link>
+            </div>
+          )
+        })}
+        
+        <div className={styles.imgWrap}>
+          <img src={IpfsParser('ipfs://bafybeigqxcwjlwmgkhg4i6sq6xptd4nfgext5kg5qxzrpap6nmzp6nwsli/img-1688047924434')} alt="Image 1"/>
+        </div>
+      </Slider>
+    </>
+  )
 }
 
 export default MainSlide;
