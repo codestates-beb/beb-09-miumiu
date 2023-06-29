4c2cc3a9f2f91a5ef8fc5f9dcf71708b11149037
leejs <leejis0212@icloud.com>
2023-06-29 17:57:02 +0900

    server 수정중 ㅠ

--- server/services/createNft.js
+++ server/services/createNft.js
@@ -1,7 +1,6 @@
 import { NFTStorage, File } from "nft.storage";
 import dotenv from "dotenv";
-import fs from "fs";
-import { ethers } from "ethers";
+import fs from "fs"
 
 dotenv.config();
 
@@ -9,39 +8,28 @@ const API_KEY = process.env.NFT_STORAGE_API_KEY;
 
 /**
  * storeNFT
- * 서명 검증 후 nft 데이터를 저장한다.
+ * nft 데이터를 저장한다.
  */
 export async function storeNFT(
-  imgPath, // @이미지 경로
-  imgName, // @이미지 이름
-  imgType, // @이미지 타입
-  title, // @제목
-  exLink, // @링크
-  description, // @설명
-  category, // @카테고리
-  signature, // @서명
-  message, // @서명된 메시지
-  userAddress // @사용자 Ethereum 주소
+  imgPath,        //@이미지 경로
+  imgName,        //@이미지 이름
+  imgType,        //@이미지 타입
+  title,          //@제목
+  exLink,         //@링크
+  description,    //@설명
+  category,       //@카테고리
+  
 ) {
-  // 서명 복구
-  const msgHash = ethers.utils.hashMessage(message);
-  const recoveredAddress = ethers.utils.verifyMessage(msgHash, signature);
-
-  // 서명한 주소와 제공된 주소가 일치하는지 확인
-  if (recoveredAddress.toLowerCase() !== userAddress.toLowerCase()) {
-    throw new Error("Invalid signature");
-  }
-
-  console.log(imgPath + imgName, imgType);
+  console.log(imgPath + imgName, imgType)
   const nft = {
     //local image 가져오기
-    image: new File([fs.readFileSync(imgPath)], imgName, { type: imgType }),
+    image: new File([fs.readFileSync(imgPath)], imgName, {type: imgType}),
     name: title,
     description: description,
     properties: {
-      exLink: exLink,
-      category: category,
-      extension: imgType, //@확장자명
+        exLink: exLink, 
+        category : category,
+        extension: imgType      //@확장자명
     },
   };
 
@@ -50,8 +38,6 @@ export async function storeNFT(
 
   console.log("NFT data stored!");
   console.log("Metadata URI: ", metadata.url);
-
-  return metadata.url;
 }
 
 //storeNFT();
