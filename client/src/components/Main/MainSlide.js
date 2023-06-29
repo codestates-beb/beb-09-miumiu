d18d724e180a76c89304845eb0a09f8955b2f40f
KimSeoYeon23 <sollucidus0221@naver.com>
2023-06-30 06:48:58 +0900

    main 수정

--- client/src/components/Main/MainSlide.js
+++ client/src/components/Main/MainSlide.js
@@ -27,7 +27,7 @@ const MainSlide = () => {
   const { state: { user }, dispatch } = useContext(Context);
   const [NftList, setNftList] = useState([]);
   const [jsonData, setJsonData] = useState([]);
-  const [NftUrl, setNftUrl] = useState()
+  const [NftUrl, setNftUrl] = useState();
   
   
 
@@ -64,83 +64,129 @@ const MainSlide = () => {
     }
     
 
-    
     // useEffect(() => {
     //   const fetchData = async (url) => {
     //     try {
     //       const response = await fetch(IpfsParser(url));
     //       const data = await response.json();
-    //       const image = data['image']
+    //       const image = data['image'];
     //       setJsonData(prevData => [...prevData, image]);
-          
     //     } catch (error) {
     //       console.error(error);
     //     }
     //   };
-    //   NftList.map((data) => {
-    //     fetchData(data[1])
-    //   })
-      
-    // }, []);
     
+    //   const fetchAllData = async () => {
+    //     for (const data of NftList) {
+    //       await fetchData(data[1]);
+    //     }
+    //     console.log("jsonData", jsonData);
+    //   };
+    
+    //   fetchAllData();
+    // }, [NftList]);
+
+    // useEffect(() => {
+    //   const fetchData = async (url) => {
+    //       try {
+    //           const response = await fetch(IpfsParser(url));
+    //           const data = await response.json();
+    //           return IpfsParser(data['image']);  // IPFS URL을 반환하도록 변경
+    //       } catch (error) {
+    //           console.error(error);
+    //       }
+    //   };
+  
+    //   const fetchAllData = async () => {
+    //       try {
+    //           const allData = await Promise.all(NftList.map(data => fetchData(data[1])));
+    //           setJsonData(allData);  // allData는 이미 IPFS URL의 배열입니다.
+    //           console.log('jsonData', allData);
+    //       } catch (error) {
+    //           console.error(error);
+    //       }
+    //   };
+  
+    //   fetchAllData();
+    // }, [NftList]);
     useEffect(() => {
       const fetchData = async (url) => {
-        try {
-          const response = await fetch(IpfsParser(url));
-          const data = await response.json();
-          const image = data['image'];
-          setJsonData(prevData => [...prevData, image]);
-        } catch (error) {
-          console.error(error);
-        }
+          try {
+              const response = await fetch(IpfsParser(url));
+              const data = await response.json();
+              return IpfsParser(data['image']);  // IPFS URL을 반환하도록 변경
+          } catch (error) {
+              console.error(error);
+          }
       };
-    
+  
       const fetchAllData = async () => {
-        for (const data of NftList) {
-          await fetchData(data[1]);
-        }
-        console.log("jsonData", jsonData);
+          try {
+              const allData = await Promise.all(NftList.map(data => fetchData(data[1])));
+              setJsonData(allData);  // allData는 'image' 속성의 값의 배열입니다.
+              console.log('jsonData', allData);
+          } catch (error) {
+              console.error(error);
+          }
       };
-    
+  
       fetchAllData();
     }, [NftList]);
+  
+    return (
+      <>
+        <Slider className={styles.mainSlide} {...settings}>
+          {jsonData.map((data, i) => {
+            console.log("data", data)
+            return (
+              <div className={styles.imgWrap} key={i}>
+                <img src={data} alt={`Image ${i}`}/>
+              </div>
+            )
+          })}
+          <div className={styles.imgWrap}>
+            <img src={IpfsParser('ipfs://bafybeigqxcwjlwmgkhg4i6sq6xptd4nfgext5kg5qxzrpap6nmzp6nwsli/img-1688047924434')} alt="Image 1"/>
+          </div>
+          {/* <div className={styles.imgWrap}>
+            <img src={image1} alt="Image 1"/>
+          </div>
+          <div className={styles.imgWrap}>
+            <img src={image1} alt="Image 1"/>
+          </div>
+          <div className={styles.imgWrap}>
+            <img src={image2} alt="Image 2" />
+          </div>
+          <div className={styles.imgWrap}>
+            <img src={image3} alt="Image 3" />
+          </div> */}
+          {/* <div className={styles.imgWrap}>
+            <img src={image3} alt="Image 3" />
+          </div> */}
+          {/* <div className={styles.imgWrap}>
+            <img src={image3} alt="Image 3" />
+          </div> */}
+        </Slider>
+      </>
+    )
+  
     
-  return (
-    <>
-      <Slider className={styles.mainSlide} {...settings}>
-        {jsonData.map((data) => {
-          console.log("data",IpfsParser(data))
-          return(
-          <div className={styles.imgWrap} key={data}>
-            <img src={IpfsParser(data)} alt="Image 1"/>
-        </div>
-          )
-        })}
-        {/* <div className={styles.imgWrap}>
-          <img src={IpfsParser('ipfs://bafybeigqxcwjlwmgkhg4i6sq6xptd4nfgext5kg5qxzrpap6nmzp6nwsli/img-1688047924434')} alt="Image 1"/>
-        </div>
-        <div className={styles.imgWrap}>
-          <img src={image1} alt="Image 1"/>
-        </div>
-        <div className={styles.imgWrap}>
-          <img src={image1} alt="Image 1"/>
-        </div>
-        <div className={styles.imgWrap}>
-          <img src={image2} alt="Image 2" />
-        </div>
-        <div className={styles.imgWrap}>
-          <img src={image3} alt="Image 3" />
-        </div>
-        <div className={styles.imgWrap}>
-          <img src={image3} alt="Image 3" />
-        </div>
-        <div className={styles.imgWrap}>
-          <img src={image3} alt="Image 3" />
-        </div> */}
-        {/* 추가 이미지 */}
-      </Slider>
-    </>
-  )
+    
+  // return (
+  //   <>
+  //     <Slider className={styles.mainSlide} {...settings}>
+  //       {jsonData.map((data, i) => {
+  //         console.log("data",IpfsParser(data))
+  //         return(
+  //         <div className={styles.imgWrap} key={i}>
+  //           <img src={IpfsParser(data)} alt="Image 1"/>
+  //       </div>
+  //         )
+  //       })}
+  //       
+  //       {/* 추가 이미지 */}
+  //     </Slider>
+  //   </>
+  // )
 }
 
 export default MainSlide;
